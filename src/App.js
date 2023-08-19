e905e9a6ec379a2e83343d3019abb463b0381a67
Rockford Lines <rockford.lines@jhuapl.edu>
2023-08-19 08:04:42 -0400

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Stack } from "@mui/material";
+import { ThemeProvider } from "@mui/system";
+import { dataVizTheme } from "./theme.ts";
+import LayoutWrapper from "./Layout/LayoutWrapper.jsx";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={dataVizTheme}>
+      <Stack className="App" height="100%">
+        <LayoutWrapper />
+      </Stack>
+    </ThemeProvider>
   );
 }
-
-export default App;
